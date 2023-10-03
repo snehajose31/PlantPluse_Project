1c1aaed3656c571761f9aafbc3e022c2cf8effdc
snehajose31 <120885046+snehajose31@users.noreply.github.com>
2023-10-03 20:38:07 +0530

    abstract

--- pjctplant/pjctplant/settings.py
+++ pjctplant/pjctplant/settings.py
@@ -125,3 +125,4 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL='plantapp.User'
