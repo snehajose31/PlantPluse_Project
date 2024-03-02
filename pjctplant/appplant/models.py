77495f2f8c0138e3084b52de6a71dc9854a73c49
snehajose31 <120885046+snehajose31@users.noreply.github.com>
2024-03-02 16:11:17 +0530

    pH_servies

--- pjctplant/appplant/models.py
+++ pjctplant/appplant/models.py
@@ -324,6 +324,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True)
 
     def __str__(self):
         return self.title
